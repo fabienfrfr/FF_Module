4924bbfec4a79fe8404b9364ae5d1a9a08536b66
fabienfrfr <69802483+fabienfrfr@users.noreply.github.com>
2021-08-29 17:41:55 +0200

    Add files via upload

--- randomgraph2net.py
+++ randomgraph2net.py
@@ -11,10 +11,12 @@ import networkx as nx
 from sklearn.cluster import Birch
 from sklearn import preprocessing
 
+import cv2
+from skimage.feature import peak_local_max
 
 ## Global parameter
 N = 300
-I, O = 100, 100
+I, O = 100, 50
 
 ########################################## CONSTRUCT GRAPH
 ## density gradient
@@ -25,10 +27,31 @@ E1 = (g/g.sum())
 ## construct density hidden grid
 X, Y = np.mgrid[0.1:3*np.pi:.1,0.1:3*np.pi:.1]
 Z = (np.sin(X)*np.cos(np.pi/2+Y))**10
-Z = E1*Z
-E = (Z/Z.sum()).T # density
+
+Z_norm = cv2.normalize(Z, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+ret, Z_mask = cv2.threshold(Z_norm,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+Z1 = (E1*Z)*Z_mask
+E = (Z1/Z1.sum()).T # density
 plt.contourf(X,Y,E); plt.show(); plt.close()
 
+## segmentation
+# Watershed 
+dist_transform = cv2.distanceTransform(Z_mask,cv2.DIST_L2,5)
+local_maxi = peak_local_max(dist_transform, indices=False, labels=Z_mask)
+ret, markers = cv2.connectedComponents(np.uint8(local_maxi))
+markers = cv2.watershed(cv2.cvtColor(Z_mask,cv2.COLOR_GRAY2BGR),markers)
+markers = markers*Z_mask
+plt.imshow(markers); plt.show(); plt.close()
+# localisation
+loc = []
+for m in np.unique(markers) :
+    loc += [[np.mean(X[markers == m]), np.mean(Y[markers == m])]]
+loc = np.array(loc)
+dist_min = []
+for l in loc :
+    dist_min += [np.linalg.norm(loc[(loc!=l).all(axis=1)]-l, axis=1).min()]
+RADIUS = max(dist_min)
 ## random variable position
 x, y = X.ravel(), Y.ravel()
 node = np.random.choice(x.size, N, p=E.ravel())
@@ -44,19 +67,19 @@ xy = np.concatenate((Input,Hidden,Output))
 pos = dict(enumerate(xy.tolist()))
 
 ## construct geometric graph
-G = nx.random_geometric_graph(N+I+O, 2.5, pos=pos)
+G = nx.random_geometric_graph(N+I+O, RADIUS, pos=pos)
 
 ## clustering
+"""
+SEGMENTATION PLUTOT
+"""
+
 min_max_scaler = preprocessing.MinMaxScaler()
 X = min_max_scaler.fit_transform(Hidden)    
 
 cluster = Birch(n_clusters = 32, threshold=X.std()/np.pi).fit(X)
 label = cluster.labels_
-
-"""
-Meilleurs solution : segmentation du pattern! (d'ailleurs mieux pour proba initiale)
-"""
-
+    
 # draw cluster with graph
 fig = plt.figure(figsize=(8, 8))
 
@@ -82,30 +105,34 @@ LABEL = np.concatenate((np.zeros(I), label+1, -1*np.ones(O))).astype(int)
 TYPE = np.zeros((N+I+O,1), dtype=int)
 TYPE[:I] = -1
 TYPE[I+N:] = 1
-# concat
-DATA = np.concatenate((IDX[:,None],XY,LABEL[:,None], TYPE), axis=1)
+# define probability connection
+PROBA = np.ones((N+I+O,1), dtype=float)
+# concat (use namedtuple ?)
+DATA = np.concatenate((IDX[:,None],XY,LABEL[:,None], TYPE, PROBA), axis=1)
 ### connect near neighbors and update type
 CONNECTED = np.array(len(DATA)*[[None]], dtype=object)
 for d in DATA[::-1]:
     # verifing if indef type or input
-    if d[-1] != -1 :
+    if d[-2] != -1 :
         # extract link
         vertice = np.fromiter(G.neighbors(d[0]), dtype=int)
         # extract data vertice
         data = DATA[vertice]
-        #print(len(data))
         # eliminate incoherent node
-        data = data[data[:,-2]!= d[-2]]
-        data = data[data[:,-1]!=1]
+        data = data[data[:,-3] != d[-3]]
+        data = data[data[:,-2] != 1]
         # calculate euclidean dist
         X = data[:,1:3].astype(float)
         if X.size != 0 :
             dist = np.linalg.norm(X-d[1:3].astype(float),axis=1)
+            # artificial proba connection (Odds ratio)
+            dist = dist*data[:,-1]
             # choose node
-            data = data[dist==dist.min()][0]
-            CONNECTED[d[0]] = data[0]
-            # update DATA TYPE
-            DATA[data[0],-1] = -1
+            data_min = data[dist==dist.min()][0]
+            CONNECTED[d[0]] = data_min[0]
+            # update DATA TYPE & Proba (+10%)
+            DATA[data_min[0],-2] = -1
+            DATA[data_min[0],-1] += .1
 
 # reconstruct graph
 H = nx.Graph()
